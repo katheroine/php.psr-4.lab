a1eeb556be51fec2939b07141578292d6bb09bbc
katheroine <katheroine@gmail.com>
2026-04-15 02:18:39 +0200

    Snapshot 2: nonexistentPathRegistrationHasNoEffect: fail 2

---
+++ src/Autoloader.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Copyright (C) 2026 Katarzyna Krasińska
+ * PHP.PSR-4.lab - https://github.com/katheroine/php.psr-4.lab
+ * Licensed under GPL-3.0 - see LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace PHPLab\StandardPSR4;
+
+class Autoloader
+{
+}
